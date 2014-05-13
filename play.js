dc414eb4bc1e984a746cf7e533e7dbc9dc240df7
Drew Botka <hellodrewbot@gmail.com>
2014-05-13 13:41:01 -0400

    Added hover state.

--- play.js
+++ play.js
@@ -125,19 +125,19 @@ console.log(shouldEat("fusia"));
 // 	}
 
 
-//prompt user input and write to DOM
-// var bananaColor = prompt("What color is your banana?");
-// if (bananaColor == "green") {
-// 		document.write("not yet, your banana is ")
-// 	} else if (bananaColor == "brown") {
-// 		document.write("better eat it before it turns black, your banana is ")
-// 	} else if (bananaColor == "black") {
-// 		document.write("yeah... better not, your banana is ")
-// 	} else if (bananaColor == "yellow")  {
-// 		document.write("Sho 'nuff, your banana is ")
-// 	} else {
-// 		document.write("That's not a banana, go wash your hands if it's ")
-// 	}
+prompt user input and write to DOM
+var bananaColor = prompt("What color is your banana?");
+if (bananaColor == "green") {
+		document.write("not yet, your banana is ")
+	} else if (bananaColor == "brown") {
+		document.write("better eat it before it turns black, your banana is ")
+	} else if (bananaColor == "black") {
+		document.write("yeah... better not, your banana is ")
+	} else if (bananaColor == "yellow")  {
+		document.write("Sho 'nuff, your banana is ")
+	} else {
+		document.write("That's not a banana, go wash your hands if it's ")
+	}
 
 // var eatTest = function(bananaColor);
 
@@ -761,6 +761,63 @@ doctorArray.reduce(function(a,b){
 	// return {yearsPlayed: a.yearsPlayed + b.yearsPlayed};
 	console.log({yearsPlayed: a.yearsPlayed + b.yearsPlayed});
 });
+
+
+
+
+// A little extra homework with .map(), .reduce(), and .forEach()
+ 
+// Show me how to calculate the average price of all items. Please console.log the average.
+// The output should be "The average price is $23.62"
+
+//Get the average price from an array of prices inside the object "items"
+function averageItemPrices(items){
+	return items.map(function(item){
+				return item.price;
+			}).reduce(function(first, second){
+				return first + second;
+			}) / item.length
+}
+
+console.log(averageItemPrices(items));
+
+// Show me how find the item with a "GBP" curreny code and print its name and price. Please console.log the one you find.
+// The output should be "1970s Schlitz Malt Liquor Glass Beer Pitcher costs £18"
+
+//forEach takes three parameters: current, index, array
+items.forEach(function(item){
+	if (item.currency_code == "GBP") {
+		console.log('found it!', item.title);
+	}
+})
+
+// BONUS: Show me how to find which items are made of wood. Please console.log the ones you find.
+// The output should be:
+// 
+// SALE Mid Century Siesta Ware White Mug with Anchor - Set of 3 is made of wood.
+// Bottle cap catcher personalized. Man cave gift for him- Wooden Beer pub sign - Groomsmen wedding Gift is made of wood.
+// Medium Size, Welcome To Our Firepit-Where Friends And Marshmallows Get Toasted At The Same Time-Painted Wood Sign-Custom Colors is made of wood.
+// Magnetic Wall Mount Bottle Opener Barware Set - Stainless Steel or Black - Personalized if you like! is made of wood.
+// Engraved Pocket Knife, Personalized Groomsmen Gift, Ring Bearer Gift, Graduation Gift, 4 Knives is made of wood.
+ 
+
+items.forEach(function(item){
+	item.materials.forEach(function(materials){
+		if (material == "wood") {
+			console.log(item.title)
+		}
+	})
+})
+
+
+
+
+//DOM Manipulation
+
+
+
+
+
 
 
 
